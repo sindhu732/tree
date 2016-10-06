afd932d17e321e0b6ad8ec73d6e0da3316af7bdd
sindhu732 <sindhu732@gmail.com>
2016-10-06 23:09:06 +0000

    new vm test

--- pytree.py
+++ pytree.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python3
-import subprocess
 import sys
+import os
 
 
-# YOUR CODE GOES here
+def gettree(arg, branch):
+    dirs = sorted(os.listdir(arg))
 
+    for i in range(0, len(dirs)):
+        path = arg + dirs[i]
+        if path[-1] != '/':
+            path_slash = path + '/'
+        filename = os.path.split(path)[-1]
+        if os.path.isdir(path):
+            if (i == len(dirs) - 1):
+                print(branch + '└── ' + filename)
+                gettree(path_slash, branch + '    ')
+            else:
+                print(branch + '├── ' + filename)
+                gettree(path_slash, branch + '│   ')
+        else:
+            if (i == len(dirs) - 1):
+                print(branch + '└── ' + filename)
+            else:
+                print(branch + '├── ' + filename)        
 
 if __name__ == '__main__':
-    # just for demo
-    subprocess.run(['tree'] + sys.argv[1:])
+    if len(sys.argv) == 1:
+        root = os.getcwd()
+        print(root)
+        gettree(root, '')
+    else:
+        root = sys.argv[1]
+        print(root)
+        gettree(root, '')
