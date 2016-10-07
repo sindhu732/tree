de8ea77cf5d2d6b2027eaadf543799a9558906eb
sindhu732 <sindhu732@gmail.com>
2016-10-07 01:22:55 +0000

    all passed git add .!

--- pytree.py
+++ pytree.py
@@ -2,31 +2,50 @@
 import sys
 import os
 
+total_dir = 0
+total_files = 0
+
+
+def listdir_nohidden(arg):
+    not_hidden = []
+    for f in sorted(os.listdir(arg), key=str.lower):
+        if not f.startswith('.'):
+            not_hidden.append(f)
+    return not_hidden
+
 
 def gettree(arg, branch):
-    dirs = sorted(os.listdir(arg))
+    dirs = listdir_nohidden(arg)
 
     for i in range(0, len(dirs)):
         path = os.path.join(arg, dirs[i])
+        global total_dir
+        global total_files
         if os.path.isdir(path):
             if (i == len(dirs) - 1):
                 print(branch + '└── ' + dirs[i])
+                total_dir += 1
                 gettree(path, branch + '    ')
             else:
                 print(branch + '├── ' + dirs[i])
+                total_dir += 1
                 gettree(path, branch + '│   ')
         else:
             if (i == len(dirs) - 1):
                 print(branch + '└── ' + dirs[i])
+                total_files += 1
             else:
-                print(branch + '├── ' + dirs[i])        
+                print(branch + '├── ' + dirs[i])
+                total_files += 1
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        root = os.getcwd()
+        root = '.'
         print(root)
         gettree(root, '')
     else:
         root = sys.argv[1]
         print(root)
         gettree(root, '')
+    print("")
+    print(total_dir, "directories,", total_files, "files")
